05bb9caf9c556f79dd9356f43b553084c1ac886b
M Farkas-Dyck <strake888@gmail.com>
2018-08-18 22:28:23 -0800

    control

--- Util/Exception.hs
+++ Util/Exception.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Util.Exception where
 
 import Control.Exception.Lifted
 import Control.Monad
-import Control.Monad.Trans.Control
+import Control.Monad.Base.Control
+import Data.Basic (Basic1 (..))
 import Data.Monoid (Alt (..))
 import Util
 
-handleJusts :: MonadBaseControl IO m => [ExceptionPredicate b] -> (b -> m a) -> m a -> m a
+handleJusts :: (MonadBaseControl m, Base m ~ IO) => [ExceptionPredicate b] -> (b -> m a) -> m a -> m a
 handleJusts = handleJust . fmap getAlt . foldMapA (fmap Alt . \ (ExceptionPredicate f) -> fromException >=> f)
 
 data ExceptionPredicate b = ∀ e . Exception e => ExceptionPredicate (e -> Maybe b)
